75258e702f59c926ee6d40e2d25e40ee0a8fa73b
KD131 <71063280+KD131@users.noreply.github.com>
2021-11-12 12:15:06 +0100

    refactors active header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import { NavLink } from "react-router-dom";
 
 function Header({ loggedIn, user }) {
+    function isActive({ isActive }) {
+        return isActive ? "active" : "";
+    }
+
     return (
         <ul className="header">
             <li><NavLink
-                className={({ isActive }) => isActive ? "active" : undefined}
+                className={isActive}
                 to="/" end>Home</NavLink></li>
             <li><NavLink
-                className={({ isActive }) => isActive ? "active" : undefined}
+                className={isActive}
                 to="/about">About</NavLink></li>
             <div className="header_right">
                 {loggedIn
@@ -15,7 +19,7 @@ function Header({ loggedIn, user }) {
                         <li><span>Hello, {user.username}! Roles: {user.roles.join(", ")}</span></li>   {/* It's only a span to avoid bootstrap's <p> styling messing up the header */}
                         <li><NavLink to="/logout">Sign out</NavLink></li>
                     </div>
-                    : <li><NavLink to="/login">Sign in</NavLink></li>
+                    : <li><NavLink className={isActive} to="/login">Sign in</NavLink></li>
                 }
             </div>
         </ul>
