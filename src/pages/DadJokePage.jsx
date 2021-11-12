bb2bb3843fcd26e037ba88ac8e38f1225424b6b9
KD131 <71063280+KD131@users.noreply.github.com>
2021-11-12 20:31:33 +0100

    adds loading message while data is fetching

--- src/pages/DadJokePage.jsx
+++ src/pages/DadJokePage.jsx
@@ -19,7 +19,9 @@ function DadJokePage() {
     return (
         <div>
             <p><i>These are fetched in parallel.</i></p>
-            {jokes && jokes.map(j => <DadJoke key={j.id} joke={j} />)}
+            {jokes
+                ? jokes.map(j => <DadJoke key={j.id} joke={j} />)
+                : <p>Loading...</p>}
         </div>
     );
 }
